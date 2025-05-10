f7575bba910413a4c631b5d7e774462161465b03
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-05-10 22:37:34 +0300

    SearchBox çalışıldı

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,25 @@ import SearchBox from "./assets/components/SearchBox";
 import ContactList from "./assets/components/ContactList";
 
 const App = () => {
-  const [contacts, setContacts ] = useState([
+  const [contacts, setContacts] = useState([
     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
   ]);
 
+  const [search, setSearch] = useState("");
+  const filteredList = contacts.filter((contact) => {
+    contact.name.toLowerCase().includes(search.toLowerCase());
+  });
+
   return (
-    <div>
+    <>
       <h1>Phonebook</h1>
       <ContactForm />
-      <SearchBox />
-      <ContactList contacts={contacts}/>
-    </div>
+      <SearchBox value={search} onChange={setSearch} />
+      <ContactList contacts={contacts} />
+    </>
   );
 };
 
--- src/assets/components/SearchBox.jsx
+++ src/assets/components/SearchBox.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import ContactList from "./ContactList";
+import Contact from "./Contact";
 
-const SearchBox = () => {
+const SearchBox = ({ value,onChange }) => {
+  const handleChange = (e) => {
+    onChange(e.target.value);
+  };
+
   return (
     <div>
-        
+      <p>Find contacts by name</p>
+      <input type="text" value={value} onChange={handleChange} />
     </div>
-  )
-}
+  );
+};
 
-export default SearchBox
+export default SearchBox;
